8145db19b5d61ee77208d6fcbb7fc2dcac9b2d8f
Anhorn <julian.anhorn@adesso.de>
2020-07-14 10:33:07 +0200

    New Layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,13 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DefaultComponent } from './layouts/default/default.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { NavComponent } from './basic-layout/nav/nav.component';
 
 
 const routes: Routes = [{
   path: '',
-  component: DefaultComponent,
-  children: [{
-    path: '',
-    component: DashboardComponent
-    }]
+  component: DashboardComponent
+
 }];
 
 @NgModule({
